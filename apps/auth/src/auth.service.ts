32d2a5566dbbeb5c667e2fe75af69169252bdcb8
Mitesh savaliya <miteshsavaliya7@gmail.com>
2024-03-26 10:02:06 +0530

    testing mail service over ci

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
+    @Inject(MAILING_SERVICE) private readonly mailingService: ClientProxy,
   ) {}
   getHello(): string {
     return 'Hello Test Auto deploy is it working!';
@@ -26,7 +27,11 @@ export class AuthService {
     );
 
     const token = this.jwtService.sign(tokenPayload);
-
+    this.mailingService.emit('mail_notify',{
+      email: user.email,
+      subject: 'Verify Email',
+      html: `<a href="http://localhost:3000/auth/verify-email?token=${token}">Click here to verify your email</a>`,
+    })
     response.cookie('Authentication', token, {
       httpOnly: true,
       expires,
